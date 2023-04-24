9b0914db72a4c27091b684bcd745bfe951508e3e
Diego Isunza <contacto.diegoisunza@gmail.com>
2023-04-23 20:06:13 -0600

    Build Navigate Card component, add ride options

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -4,8 +4,13 @@ import tw from "tailwind-react-native-classnames";
 import NavOptions from '../components/NavOptions';
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 import {GOOGLE_MAPS_APIKEY} from "@env";
+import { useDispatch } from 'react-redux';
+import { setDestination, setOrigin } from '../slices/navSlice';
 
 const HomeScreen = () => {
+  
+  const dispatch = useDispatch();
+
   return (
     <SafeAreaView style={tw`bg-white h-full`}> 
       <View style={tw`p-5`}>
@@ -30,14 +35,28 @@ const HomeScreen = () => {
               fontSize: 18,
             },
           }}
+          onPress={(data, details = null) => {
+          
+            dispatch(
+              setOrigin({
+              location: details.geometry.location,
+              description: data.description
+              })
+            );
+
+            dispatch(setDestination(null));
+          }}
+          fetchDetails={true}
+          returnKeyType={"search"}
           query={{
             key:GOOGLE_MAPS_APIKEY,
             language: 'en'
           }}
+          enablePoweredByContainer={false}
+          minLength={2}
           nearbyPlacesAPI="GooglePlacesSearch"
           debounce={400}
         />
-
         <NavOptions/>
       </View>
     </SafeAreaView>
